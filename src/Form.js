d6ab5a6e6f20d017d53d1148239b968a45ce102d
Shahith Raj Mohammed <ShahithRaj.Mohammed@agilysys.com>
2023-04-20 04:50:17 -0700

    adding recursive function

--- src/Form.js
+++ src/Form.js
@@ -14,26 +14,31 @@ const Form = ({data,maxRow}) => {
      useEffect(() => {
       updateJson(updatedObj)
      },[])
-
-     const updateJson = (obj) => {
-      for(let i=0;i<obj.Fields.length;i++) {
-     let endCount = 0;
-          if(obj?.Fields[i]?.Fields?.length > 0) {
-              for(let j=0;j<obj.Fields[i].Fields.length;j++) {
-                  let field = obj.Fields[i].Fields
-                  if(typeObj[field[j].Type]) {
-                      addingClassName(j,endCount,field,"desc")
+let endCount = 0;
+     function updateJson (obj,k) {
+      if(obj.Fields.length > 0) {
+          obj.Fields.forEach((field,i) => {
+            if(field?.Fields?.length) {
+              endCount = 0;
+              updateJson(field,1)
+            }
+            else {
+              if(k === 1) {
+                  if(typeObj[field.Type]) {
+                      addingClassName(i,endCount,obj,"desc")
                       endCount = 0;
-                  }else {
-                  endCount++;
-                if(obj.Fields[i].Fields.length - 1 === j) {
-                  addingClassName(j,endCount,field,"input")
-              }
+                  }
+                  else {
+                    endCount++;
+                    if(obj?.Fields?.length - 1 === i) {
+                      addingClassName(i,endCount,obj,"input")
+                  }
                   }
               }
           }
+          })
       }
-  }
+  } 
 
     const addingClassName = (i,count,data,type) => {
       
@@ -43,10 +48,19 @@ const Form = ({data,maxRow}) => {
       }
       if(count !== 0) {
           let rem = count % maxRow;
-          for(let j=0;j<rem;j++) {
-              let className = data[i - j - val]?.className
-              data[i - j - val].className = className ? className + " input" + rem : "input" + rem;
-          }
+          if(rem === 3) {
+            let className = data.Fields[i - val]?.CssClass
+            for(let j=0;j<rem-1;j++) {
+                data.Fields[i - j - val - 1].CssClass = className ? className + " input" + 2 : "input" + 2;
+            }
+            data.Fields[i - val].CssClass = className ? className + " input" + 1 : "input" + 1;
+        }
+        else {
+            for(let j=0;j<rem;j++) {
+                let className = data.Fields[i - j - val]?.CssClass
+                data.Fields[i - j - val].CssClass = className ? className + " input" + rem : "input" + rem;
+            }
+        }
       }
       setObj(updatedObj)
    }
